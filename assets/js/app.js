ad8595b721a0756e7ac51f00a256e56f4645c3c2
abhi9720 <abhishektiwari9720@gmail.com>
2022-01-17 00:21:08 +0530

    css set

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+(function () {
+
+
+    // let nav = document.querySelector('.navbar-nav')
+    // nav.ready(function () {
+    //     document.querySelector('a.active').removeClass('active');
+    //     document.querySelector('a[href="' + location.pathname + '"]').closest('li').classList.add('active');
+    // });
+
+
+
+})()
