a304f59ad847b1a0ca24216d3215e80dd1e9dcfa
abhi9720 <abhishektiwari9720@gmail.com>
2022-01-17 12:06:32 +0530

    search bar

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,22 +1,25 @@
 const { profile } = require('console');
 const User = require('../models/user')
 
-module.exports.home = function(req,res){
+module.exports.home = function (req, res) {
     // return res.render('user_profile',{
     //     title : 'ProgrammersTODO'
     // })
-    
 
-    if(!req.user){
+    var userIP = req.connection.remoteAddress;
+    console.log(userIP + " connected to the site.");
+
+
+    if (!req.user) {
         return res.redirect('/users/signin');
     }
-    else{
+    else {
         // console.log(req.user);
-        return res.render('user_profile',{
-            title : 'Home',
-            page_name : 'profile',
-            profile_user : req.user
+        return res.render('user_profile', {
+            title: 'Home',
+            page_name: 'profile',
+            profile_user: req.user
         })
     }
-    
+
 }
