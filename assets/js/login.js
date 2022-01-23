c8848790d282b7997f8e237b1e79714707714d95
abhi9720 <abhishektiwari9720@gmail.com>
2022-01-23 14:47:49 +0530

    new ui changes

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,30 @@
 
 function guestLogin() {
+    let email = "guestuser@gmail.com";
+    let index = 0;
     const loginForm = document.forms[0];
     let emailinput = document.getElementById('email')
     let passwordinput = document.getElementById('password')
     emailinput.focus();
-    passwordinput.focus();
-    emailinput.value = "guestuser@gmail.com";
-    passwordinput.value = "12345";
-    loginForm.submit()
+
+    const myInterval = setInterval(myTimer, 50);
+
+    function myTimer() {
+        if (index < email.length) {
+            emailinput.value += email[index]
+            index++;
+        }
+        else {
+            clearInterval(myInterval)
+            passwordinput.focus();
+            passwordinput.value = "12345";
+            loginForm.submit()
+        }
+    }
 }
+
+
+
+
+
+
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -77,6 +77,7 @@ module.exports.signup = function (req, res) {
 
 module.exports.signin = function (req, res) {
     // console.log("logged in");
+    console.log(req.body);
     if (req.isAuthenticated()) {
         return res.redirect(`/`);
     }
